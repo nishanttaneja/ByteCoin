b5ea2c4faa970922b8c8ef85660ee92fe803ee3f
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-31 16:34:43 +0530

    API Key

---
+++ ByteCoin/Models/PrivateData.swift
@@ -0,0 +1,13 @@
+//
+//  PrivateData.swift
+//  ByteCoin
+//
+//  Created by Nishant Taneja on 31/08/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import Foundation
+
+struct PrivateData {
+    static let apiKey: String = "your_api_key"
+}

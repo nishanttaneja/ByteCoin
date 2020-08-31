185a5ad261f0ebd14fd1f0c5a044d43c4f4a82fb
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-31 14:06:37 +0530

    implement CoinManager

--- ByteCoin/ViewController.swift
+++ ByteCoin/ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
+    // Initialise
+    private var coinManager = CoinManager()
+    private var currencies = [String]()
+    
     // Override View Method
     override func viewDidLoad() {
         super.viewDidLoad()
         // CurrencyPicker DataSource|Delegate
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
+        // CoinManagerDelegate
+        coinManager.delegate = self
+        currencies = coinManager.getCurrencies()
     }
 }
 
@@ -27,10 +34,21 @@ class ViewController: UIViewController {
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     // DataSource Methods
     func numberOfComponents(in pickerView: UIPickerView) -> Int {return 1}
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {}
-    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {return currencies.count}
     // Delegate Methods
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {}
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {}
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {return currencies[row]}
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.fetchRate(for: currencies[row])
+    }
 }
 
+//MARK:- CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
+    func didFetchRate(_ rate: Double, for currency: String) {
+        DispatchQueue.main.async {
+            self.rateLabel.text = String(format: "%.2f", rate)
+            self.currencyLabel.text = currency
+        }
+    }
+    func didFailWithError(_ error: Error) {print(error)}
+}

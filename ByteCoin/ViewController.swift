dd0a0f8c391e8aafa62a30c3a6e525316c39ebc9
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-31 12:46:49 +0530

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Nishant Taneja on 31/08/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

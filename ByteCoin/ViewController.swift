37fa4b25c47796e7a486e48a8ae08be9dddfb8a7
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-31 13:12:46 +0530

    add CurrencyPickerView Delegate|DataSource

--- ByteCoin/ViewController.swift
+++ ByteCoin/ViewController.swift
@@ -9,8 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // IBOutlets
     @IBOutlet weak var rateLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    // Override View Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // CurrencyPicker DataSource|Delegate
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+}
+
+//MARK:- CurrencyPickerView DataSource|Delegate
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    // DataSource Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {return 1}
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {}
+    
+    // Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {}
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {}
 }
 
